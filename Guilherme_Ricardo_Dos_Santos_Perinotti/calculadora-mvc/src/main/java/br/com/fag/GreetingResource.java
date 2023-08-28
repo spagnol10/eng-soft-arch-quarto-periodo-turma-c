09d96caaa0d9cccb9bcded0386d8036df18ddfd8
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-28 16:45:55 -0300

    adicionando template

--- Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/GreetingResource.java
+++ Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/GreetingResource.java
@@ -9,7 +9,7 @@ import jakarta.ws.rs.core.MediaType;
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_HTML)
     public String hello() {
         return "Hello RESTEasy";
     }
---
+++ Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/model/EnumUserOption.java
@@ -0,0 +1,12 @@
+package br.com.fag.model;
+
+public enum EnumUserOption {
+  ADICAO,
+  SUBTRACAO,
+  MULTIPLICACAO,
+  DIVISAO;
+
+  public Object getValue() {
+    return null;
+  }
+}
