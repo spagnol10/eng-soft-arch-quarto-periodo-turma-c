1e216792e99c49d2a739963502f1833df7c4bb2c
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-08-29 11:06:20 -0300

    Adicionando controllers para lidar com a requisição

--- Allysom_Renczenczen/Calculadora/src/main/java/br/fag/controllers/CaculatorResource.java
+++ Allysom_Renczenczen/Calculadora/src/main/java/br/fag/controllers/CaculatorResource.java
@@ -1,4 +1,8 @@
-package br.fag.Controllers;
+package br.fag.controllers;
+import br.fag.services.Divisao;
+import br.fag.services.Multiplicacao;
+import br.fag.services.Somar;
+import br.fag.services.Substracao;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
@@ -6,18 +10,17 @@ import jakarta.ws.rs.core.Response;
 @Path("/calculate")
 public class CaculatorResource {
     @GET
-    public Response calculate(@QueryParam("fisrt") float first, @QueryParam("second") float second, @QueryParam("operator") String operator){
-        float result;
+    public Response calculate(@QueryParam("first") float first, @QueryParam("second") float second, @QueryParam("operator") String operator){
+        Divisao divisao = new Divisao();
+        Somar somar = new Somar();
+        Substracao substracao = new Substracao();
+        Multiplicacao multiplicacao = new Multiplicacao();
+        Response result;
         switch (operator) {
-            case "+" -> result = first + second;
-            case "-" -> result = first - second;
-            case "*" -> result = first * second;
-            case "/" -> {
-                if (second == 0) {
-                    return Response.status(Response.Status.BAD_REQUEST).entity("Cannot divide by zero").build();
-                }
-                result = first / second;
-            }
+            case "+" -> result = somar.operacao(first, second);
+            case "-" -> result = substracao.operacao(first,second);
+            case "*" -> result = multiplicacao.operacao(first,second);
+            case "/" -> result = divisao.operacao((int)first,(int)second);
             default -> {
                 return Response.status(Response.Status.BAD_REQUEST).entity("Invalid operator").build();
             }
