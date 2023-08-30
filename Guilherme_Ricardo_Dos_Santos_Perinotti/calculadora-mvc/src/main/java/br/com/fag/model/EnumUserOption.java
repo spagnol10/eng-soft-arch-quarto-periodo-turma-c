8ebbe807c4c408139a5f008fb75304f94668cc5b
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-08-30 14:04:22 -0300

    modified enum

--- Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/model/EnumUserOption.java
+++ Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/model/EnumUserOption.java
@@ -1,12 +1,28 @@
 package br.com.fag.model;
 
 public enum EnumUserOption {
-  ADICAO,
-  SUBTRACAO,
-  MULTIPLICACAO,
-  DIVISAO;
+  ADICAO(1, "Adição"),
+  SUBTRACAO(2, "Subtração"),
+  MULTIPLICACAO(3, "Multiplicação"),
+  DIVISAO(4, "Divisão");
 
-  public Object getValue() {
-    return null;
+  private Integer key;
+  private String value;
+
+  private EnumUserOption(Integer key, String value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  public Integer getKey() {
+    return this.key;
+  }
+
+  public String getValue() {
+    return this.value;
+  }
+
+  public EnumUserOption getOption() {
+    return this;
   }
 }
