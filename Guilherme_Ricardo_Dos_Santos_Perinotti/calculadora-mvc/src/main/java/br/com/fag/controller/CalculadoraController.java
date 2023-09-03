dd3c2b2c86aaeda423d92e7471109b81bfdb631b
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-09-03 16:01:31 -0300

    final

--- Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/controller/CalculadoraController.java
+++ Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/controller/CalculadoraController.java
@@ -1,23 +1,24 @@
 package br.com.fag.controller;
 
-import io.quarkus.qute.Template;
-import io.quarkus.qute.TemplateInstance;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.MediaType;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 import br.com.fag.model.EnumUserOption;
+import br.com.fag.service.CalculadoraService;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Consumes(MediaType.TEXT_HTML)
 @Produces(MediaType.TEXT_HTML)
-@Path("/calculadora")
+@Path("/")
 public class CalculadoraController {
   
   @Inject
@@ -36,14 +37,16 @@ public class CalculadoraController {
     return index.data("name", "Guilherme Perinotti", "arithmeticOperations", arithmeticOperations);
   }
 
-  // @POST
-  // public TemplateInstance post(@QueryParam("name") String name) {
-  //   List<EnumUserOption> arithmeticOperations = List.of(EnumUserOption.values())
-  //     .stream()
-  //     .map(e -> e.getOption())
-  //     .collect(Collectors.toList());
+  @POST
+  @Consumes("application/x-www-form-urlencoded")
+  public TemplateInstance post(@FormParam("primeiroValor") Float primeiro, @FormParam("segundoValor") Float segundo, @FormParam("selectCalculadora") Integer operation ) {
+
+    CalculadoraService service = new CalculadoraService();
+
+    Float result = service.calculate(primeiro, segundo, operation);
+
     
-  //   return index.data("name", name, "arithmeticOperations", arithmeticOperations);
-  // }
+    return calculadora.data("result", result);
+  }
 
 }
--- Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/service/CalculadoraService.java
+++ Guilherme_Ricardo_Dos_Santos_Perinotti/calculadora-mvc/src/main/java/br/com/fag/service/CalculadoraService.java
@@ -1,5 +1,24 @@
 package br.com.fag.service;
 
 public class CalculadoraService {
-  
+  public Float calculate(Float primeiro, Float segundo, Integer operation) {
+    switch(operation) {
+      case 1:
+        return primeiro + segundo;
+
+      case 2:
+      return primeiro - segundo;
+
+      case 3:
+      return primeiro * segundo;
+
+      case 4:
+      if(segundo == 0) {
+        throw new ArithmeticException("Não pode dividir por zero");
+      }
+      return primeiro / segundo;
+    }
+
+    return 0f;
+  }
 }
