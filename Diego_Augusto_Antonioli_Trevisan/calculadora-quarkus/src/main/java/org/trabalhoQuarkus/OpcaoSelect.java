4b43f5cbe1093353c81d69157ad2b7ddcba85666
DaaTrevisan <daatrevisan@minha.fag.edu.br>
2023-09-13 18:54:12 -0300

    Calculos funcionando sem CSS

---
+++ Diego_Augusto_Antonioli_Trevisan/calculadora-quarkus/src/main/java/org/trabalhoQuarkus/OpcaoSelect.java
@@ -0,0 +1,19 @@
+package org.trabalhoQuarkus;
+
+public class OpcaoSelect {
+    private String valor;
+    private String texto;
+
+    public OpcaoSelect(String valor, String texto) {
+        this.valor = valor;
+        this.texto = texto;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+}
---
+++ Diego_Augusto_Antonioli_Trevisan/calculadora-quarkus/src/main/java/org/trabalhoQuarkus/SomePage.java
@@ -0,0 +1,61 @@
+package org.trabalhoQuarkus;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import org.jboss.resteasy.annotations.jaxrs.QueryParam;
+@Path("/SomePage")
+public class SomePage{
+
+    @Inject
+    Template SomePage;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance SomePage(@QueryParam String param) {
+        List<OpcaoSelect> operacao = new ArrayList<OpcaoSelect>();
+        operacao.add(new OpcaoSelect("soma", "Soma"));
+        operacao.add(new OpcaoSelect("subtracao", "Subtração"));
+        operacao.add(new OpcaoSelect("multiplicacao", "Multiplicação"));
+        operacao.add(new OpcaoSelect("divisao", "Divisão"));
+
+        return SomePage.data("operacao", "operacao");
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance post(
+        @FormParam("operacao") String operacao,
+        @FormParam("n1") Double n1,
+        @FormParam("n2") Double n2) {
+        Double result = 0.0;
+        switch (operacao) {
+            case "soma":
+                result = n1 + n2;
+                break;
+            case "subtracao":
+                result = n1 - n2;
+                break;
+            case "multiplicacao":
+                result = n1 * n2;
+                break;
+            case "divisao":
+                if (n2 != 0) {
+                    result = n1 / n2;
+                }
+                break;
+            default:
+                break;
+        }
+        return SomePage.data("result", result);
+    }
+
+}
