785c39da435214665b662fa2ad5ae480b8e50235
Mateus Pfeffer <mateus.pfeffer@makertec.com.br>
2023-09-18 13:35:59 -0300

    Validação da equação movida para o CalculadoraService

--- Mateus_Pfeffer/calculadora-mvc/src/main/java/com/fag/service/CalculadoraService.java
+++ Mateus_Pfeffer/calculadora-mvc/src/main/java/com/fag/service/CalculadoraService.java
@@ -10,14 +10,24 @@ import java.math.RoundingMode;
 public class CalculadoraService {
 
     public BigDecimal calculate(BigDecimal num1, BigDecimal num2, EnumOperation operation) {
-        BigDecimal result = switch (operation) {
-            case ADICAO -> num1.add(num2);
-            case SUBTRACAO -> num1.subtract(num2);
-            case MULTIPLICACAO -> num1.multiply(num2);
-            case DIVISAO -> num1.divide(num2, 2, RoundingMode.HALF_UP);
-        };
+        boolean isValidEquation = this.validateEquation(num2, operation);
 
-        return result.setScale(2, RoundingMode.HALF_UP);
+        if (!isValidEquation) {
+            BigDecimal result = switch (operation) {
+                case ADICAO -> num1.add(num2);
+                case SUBTRACAO -> num1.subtract(num2);
+                case MULTIPLICACAO -> num1.multiply(num2);
+                case DIVISAO -> num1.divide(num2, 2, RoundingMode.HALF_UP);
+            };
+
+            return result.setScale(2, RoundingMode.HALF_UP);
+        } else {
+            throw new ArithmeticException("Não é possível dividir por zero!");
+        }
+    }
+
+    public boolean validateEquation(BigDecimal num2, EnumOperation operation) {
+        return num2.equals(BigDecimal.ZERO) && operation.equals(EnumOperation.DIVISAO);
     }
 
 }
