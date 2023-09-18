122689973b2893a3edc01c208e1ed55b4289df49
FelipeLedur <51247001+FelipeLedur@users.noreply.github.com>
2023-09-18 20:34:56 -0300

    Update

--- Felipe_Antunes_Ledur/calculadoramvc/src/main/java/org/acme/service/CalculatorService.java
+++ Felipe_Antunes_Ledur/calculadoramvc/src/main/java/org/acme/service/CalculatorService.java
@@ -10,7 +10,9 @@ public class CalculatorService {
         } else if (operacao == 3) {
             resultado = num1 * num2;
         } else if (operacao == 4) {
-           
+           if (num2 ==0){
+                throw new IllegalArgumentException("Nao e possivel dividir por 0");
+           }
             resultado = num1 / num2;
         }
         return resultado;
