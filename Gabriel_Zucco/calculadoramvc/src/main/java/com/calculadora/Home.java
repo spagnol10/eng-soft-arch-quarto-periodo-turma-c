bf6a399d743c6e5650207210dbc4da4132dbaf3e
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2023-09-19 01:31:58 -0300

    Alterações para funcionamento

---
+++ Gabriel_Zucco/calculadoramvc/src/main/java/com/calculadora/Home.java
@@ -0,0 +1,29 @@
+package com.calculadora;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import static java.util.Objects.requireNonNull;
+
+@Path("/")
+public class Home {
+
+    private final Template page;
+
+    public Home(Template page) {
+        this.page = requireNonNull(page, "page is required");
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(@QueryParam("resultado") String r) {
+        return page.data("resultado", r).data("init", "block").data("res", "none");
+
+    }
+
+}
