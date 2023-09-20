b53c61fd6fdbc9392a84d2d72a0f722ce7534034
LeonardoX7 <leonardo_m.o14@hotmail.com>
2023-09-19 21:35:01 -0300

    Ajustando casas decimais e campos numéricos

--- Leonardo_de_Meira_Oliveira/Calculator/src/main/java/com/fag/model/operacoes.java
+++ Leonardo_de_Meira_Oliveira/Calculator/src/main/java/com/fag/model/operacoes.java
@@ -1,17 +1,18 @@
 package com.fag.model;
 
+import java.text.DecimalFormat;
+
 public class operacoes {
 
     public operacoes() {}
 
-    public static Float getOperacao(Float valor1, Float valor2, Character operacao) {
+public static String getOperacao(Float valor1, Float valor2, Character operacao) {
         if (valor1 == null || valor2 == null || operacao == null) {
-            System.out.println("Por favor, preencha todos os campos!");
-            return null;
+            return "Por favor, preencha todos os campos!";
         }
-    
+
         float resultado = 0;
-    
+
         switch (operacao) {
             case '+':
                 resultado = valor1 + valor2;
@@ -24,16 +25,17 @@ public class operacoes {
                 break;
             case '/':
                 if (valor2 == 0) {
-                    System.out.println("Não é possível dividir por zero.");
-                    return null; 
+                    return "Não é possível dividir por zero.";
                 }
                 resultado = valor1 / valor2;
                 break;
             default:
-                System.out.println("Operação inválida!");
-                return null;
+                return "Operação inválida!";
         }
-    
-        return resultado;
-    }    
+
+        DecimalFormat formato = new DecimalFormat("0.00");
+        String resultadoFormatado = formato.format(resultado);
+
+        return resultadoFormatado;
+    }
 }
