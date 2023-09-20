7a56091d24044b08cbb4078ca7378cbb384d4aa8
Mateus Pfeffer <mateuspfeffer@gmail.com>
2023-09-20 06:41:32 -0300

    Criado repository para Equacao

---
+++ Mateus_Pfeffer/calculadora-mvc/src/main/java/com/fag/repository/EquacaoRepository.java
@@ -0,0 +1,9 @@
+package com.fag.repository;
+
+import com.fag.model.Equacao;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class EquacaoRepository implements PanacheRepository<Equacao> {
+}
